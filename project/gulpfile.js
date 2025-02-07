6b826cd1b6a4dd53085e60c8f8bade4251c11cb8
Cristina Grosso <134479800+cristinacoding@users.noreply.github.com>
2025-02-07 13:03:01 +0000

    added watch task in gulpfile.js to watch for changes in sass files

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,16 +1,21 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));  // Import sass with gulp-sass
 
-function compileSass() {
-  return gulp.src('src/scss/**/*.scss') // Path to your SCSS files
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css')); // Output directory for CSS files
-}
-gulp.task('sass', compileSass);
+// Path to your Sass files
+const sassFiles = 'src/scss/**/*.scss';  // Adjust this path based on your project structure
+const outputDir = 'dist/css';  // Directory to output the compiled CSS
 
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+// Sass task
+gulp.task('sass', () => {
+  return gulp.src(sassFiles)  // Grab all .scss files
+    .pipe(sass().on('error', sass.logError))  // Compile Sass to CSS
+    .pipe(gulp.dest(outputDir));  // Output the compiled CSS
+});
+
+// Watch task to watch for changes in Sass files
+gulp.task('watch', () => {
+  gulp.watch(sassFiles, gulp.series('sass'));  // Watch for changes in .scss files and run the 'sass' task
+});
+
+// Default task that runs when you just type 'gulp'
+gulp.task('default', gulp.series('sass', 'watch'));
